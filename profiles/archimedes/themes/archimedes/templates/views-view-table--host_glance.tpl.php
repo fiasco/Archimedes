2478d72ecc1208545d3f75f752be30bef318f408
Brendan Vercoelen <brendanv@catalyst.net.nz>
2011-02-03 16:22:26 +1300

    Made all host info visible even when blank

--- profiles/archimedes/themes/archimedes/templates/views-view-table--host_glance.tpl.php
+++ profiles/archimedes/themes/archimedes/templates/views-view-table--host_glance.tpl.php
@@ -1,8 +1,6 @@
 <?php
   foreach ($rows[0] as $key => $value) {
-    if (!empty($value)) {
-      $rows_d[] = array(array('header' => 1, 'data' => $header[$key]), $value);
-    }
+    $rows_d[] = array(array('header' => 1, 'data' => $header[$key]), $value);
   }
   print theme('table',null,$rows_d);
 ?>
