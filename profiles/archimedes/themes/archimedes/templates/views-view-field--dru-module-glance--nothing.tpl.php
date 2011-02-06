8bd134d98d82bcd8b97773fdd5933c02cd19140c
Brendan Vercoelen <brendanv@catalyst.net.nz>
2011-02-07 11:06:59 +1300

    Added views template for latest module version

---
+++ profiles/archimedes/themes/archimedes/templates/views-view-field--dru-module-glance--nothing.tpl.php
@@ -0,0 +1,27 @@
+<?php
+  module_load_include('inc', 'archimedes_drupal_site', 'archimedes_drupal_site.update');
+  $vocab = taxonomy_get_vocabularies($type = NULL);
+  $recommended = array();
+  foreach ($vocab as $v) {
+    if ($v->name == 'Drupal Major Version') {
+       $vid = $v->vid;
+    }
+  }
+  if ($vid) {
+    $proj = $row->node_data_field_dru_proj_field_dru_proj_value;
+    $terms = taxonomy_get_tree($vid);
+    foreach ($terms as $version) {
+      if ($data = cache_get($proj . $version->name, 'cache_archimedes_drupal')) {
+        $latest = reset($data->data['releases']);
+        $recommended[] = $latest['version'];
+      }
+    }
+  }
+  if (!empty($recommended)) {
+    $output = implode(', ', $recommended);  
+  }
+  else {
+    $output = t('Could not accurately determine module version status.');
+  }
+?>
+<?php print $output; ?>
